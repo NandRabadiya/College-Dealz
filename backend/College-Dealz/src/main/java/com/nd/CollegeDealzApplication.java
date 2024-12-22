dfbf424a0cbdc8b7a5de0a0b997a7596fa1b0c6f
Nand Rabadiya <nandrabadiya2003@gmail.com>
2024-12-22 21:55:38 +0530

    Spring boot and React Setup

---
+++ backend/College-Dealz/src/main/java/com/nd/CollegeDealzApplication.java
@@ -0,0 +1,13 @@
+package com.nd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollegeDealzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollegeDealzApplication.class, args);
+	}
+
+}
