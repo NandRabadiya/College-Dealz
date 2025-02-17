bbc4105590a25cc5813de731cef017aa0934bf75
Nand Rabadiya <nandrabadiya2003@gmail.com>
2025-02-17 19:01:18 +0530

    Configuration Changes 2.

---
+++ backend/College-Dealz/src/main/java/com/nd/config/EnvConfig.java
@@ -0,0 +1,9 @@
+package com.nd.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:.env")
+public class EnvConfig {
+}
