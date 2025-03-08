8128839dce4101886924c07e7b8b5e511517d2e5
Nand Rabadiya <nandrabadiya2003@gmail.com>
2025-03-08 09:15:54 +0530

    Deployment changes 2

--- backend/College-Dealz/src/main/java/com/nd/config/OAuthAuthenticationSuccessHandler.java
+++ backend/College-Dealz/src/main/java/com/nd/config/OAuthAuthenticationSuccessHandler.java
@@ -175,8 +175,8 @@ logger.info("OAuthAuthenicationSuccessHandler: Google New user check before user
             response.setContentType("application/json");
             response.setStatus(HttpServletResponse.SC_OK);
             response.getWriter().write(jsonResponse);
-             final String frontendRedirectUrl = "http://localhost:5173/oauth-callback";
-       //      final String frontendRedirectUrl = "https://college-dealz.vercel.app/oauth-callback";
+          //   final String frontendRedirectUrl = "http://localhost:5173/oauth-callback";
+          final String frontendRedirectUrl = "https://college-dealz.vercel.app/oauth-callback";
             response.sendRedirect(frontendRedirectUrl + "?token=" + accessToken);
         }
 
