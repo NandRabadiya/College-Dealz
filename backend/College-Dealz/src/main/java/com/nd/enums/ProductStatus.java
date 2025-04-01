9dd31eb0273ed35f6c20c6bcda37dc2cede7a33f
Nand Rabadiya <nandrabadiya2003@gmail.com>
2025-04-01 17:23:36 +0530

    Deployment changes

--- backend/College-Dealz/src/main/java/com/nd/enums/ProductStatus.java
+++ backend/College-Dealz/src/main/java/com/nd/enums/ProductStatus.java
@@ -1,8 +1,7 @@
 package com.nd.enums;
 
 public enum ProductStatus {
-    RESERVED,
-    SOLD_VIA_PLATFORM,
+     SOLD_VIA_PLATFORM,
     SOLD_OUTSIDE_PLATFORM,
     REMOVED_BY_USER,
     REMOVED_BY_ADMIN,
